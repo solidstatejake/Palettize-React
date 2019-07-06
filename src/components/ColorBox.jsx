bbfd9a74d0c9808d442c596b0ad747786bf7b05e
solidstatejake <jberg3@binghamton.edu>
2019-07-06 18:55:24 -0400

    Style color boxes: setup css grid for individual ColorBox components, and then a secondary grid for the elements inside of each ColorBox.

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -5,13 +5,8 @@ class ColorBox extends Component {
     const { name, background } = this.props;
     return (
       <div className="ColorBox" style={ { background } }>
-        <div className="ColorBox__copy--container">
-          <div className="ColorBox__content">
-
-            <span className='ColorBox__content--color-name'>{ name }</span>
-          </div>
-          <button className="ColorBox__button--copy">Copy</button>
-        </div>
+        <button className="ColorBox__button--copy">Copy</button>
+        <span className='ColorBox__content--color-name'>{ name }</span>
         <span className='ColorBox__button--see-more'>More</span>
       </div>
     );
