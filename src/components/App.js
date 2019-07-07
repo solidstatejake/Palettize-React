ef617f1cc2848c266acfb4052bed2054c2685550
solidstatejake <jberg3@binghamton.edu>
2019-07-07 19:01:34 -0400

    Integrate react-router-dom package and create functionality for routing user to palettes by their id (slugged name of palette).

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Palette from "./Palette";
 import seedColors from "../utils/seedColors";
-import {generatePalette} from "../utils/colorHelpers";
+import { generatePalette } from "../utils/colorHelpers";
 import '../stylesheets/css/main.css';
 
 class App extends Component {
+  findPalette(id) {
+    return seedColors.find((palette) => {
+      return palette.id === id;
+    })
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[ 1 ])} />
-      </div>
+      <Router>
+        <Switch>
+
+          <Route
+            exact
+            path='/'
+            render={ () => <Link to='/palette/flat-ui-colors-dutch'>DUTCHIE</Link> }
+
+          />
+
+
+          <Route
+            exact
+            path='/palette/:id'
+            render={ (routeProps) => (
+               <Palette
+                 palette={
+                   generatePalette(this.findPalette(routeProps.match.params.id))
+                 }
+               />
+            )}
+          />
+
+
+        </Switch>
+      </Router>
+      // <div className="App">
+      //   <Palette palette={ generatePalette(seedColors[ 1 ]) }/>
+      // </div>
     );
   }
 }
