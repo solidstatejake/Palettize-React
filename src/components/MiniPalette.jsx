e25c2a0ce107267866e2c2e1f51e21951989f81e
solidstatejake <jberg3@binghamton.edu>
2019-07-09 15:19:26 -0400

    Style clear palette button in Drawer component.

--- src/components/MiniPalette.jsx
+++ src/components/MiniPalette.jsx
@@ -35,7 +35,7 @@ const styles = {
 
 
 function MiniPalette(props) {
-  const { classes, colors, emoji, id, paletteName } = props;
+  const { classes, colors, emoji, paletteName } = props;
   const miniColorBoxes = colors.map(color =>
     <div
       key={color.color}
