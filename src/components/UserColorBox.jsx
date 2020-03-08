60b82e437a53627c4a72f33c13233c67d054e110
solidstatejake <jberg3@binghamton.edu>
2020-03-08 01:50:20 -0500

    Reformatting.

--- src/components/UserColorBox.jsx
+++ src/components/UserColorBox.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { SortableElement } from 'react-sortable-hoc';
-import { withStyles } from '@material-ui/styles';
+import React               from 'react'
+import { SortableElement } from 'react-sortable-hoc'
+import { withStyles }      from '@material-ui/styles'
+
 
 const styles = {
   root: {
-    display: 'grid',
-    cursor: 'grab',
-    fontSize: '1rem',
-    gridTemplateRows: '17rem 3rem',
+    display:             'grid',
+    cursor:              'grab',
+    fontSize:            '1rem',
+    gridTemplateRows:    '17rem 3rem',
     gridTemplateColumns: '17rem 3rem',
-    gridTemplateAreas: '". ." "title trash"',
-    height: '20rem',
-    letterSpacing: '.2rem',
-    textTransform: 'uppercase',
-    width: '20rem',
+    gridTemplateAreas:   '". ." "title trash"',
+    height:              '20rem',
+    letterSpacing:       '.2rem',
+    textTransform:       'uppercase',
+    width:               '20rem',
 
     '&:active': {
-        cursor: 'move'
+      cursor: 'move'
     },
 
     '&:focus-within': {
@@ -24,18 +25,18 @@ const styles = {
     },
 
     '& span': {
-      gridArea: 'title',
+      gridArea:   'title',
       marginLeft: '1rem'
     },
 
     '& div': {
-      gridArea: 'trash',
-      textAlign: 'center',
+      gridArea:   'trash',
+      textAlign:  'center',
       lineHeight: '3rem',
 
       '& i': {
-        fontSize: '1.3rem',
-        cursor: 'pointer',
+        fontSize:   '1.3rem',
+        cursor:     'pointer',
         transition: '.3s',
 
         '&:hover': {
@@ -44,24 +45,20 @@ const styles = {
       }
     }
   }
-};
+}
 
-function UserColorBox(props) {
-
-  const { name, background, classes, deleteUserColorBox } = props;
+function UserColorBox (props) {
+  const { name, background, classes, deleteUserColorBox } = props
   return (
-    <div className={ classes.root }
-         style={ { background } }
-    >
+    <div className={ classes.root } style={ { background } }>
 
       <span>{ name }</span>
-
-      <div onClick={deleteUserColorBox}>
-        <i className='icon-trash'></i>
+      <div onClick={ deleteUserColorBox }>
+        <i className='icon-trash'/>
       </div>
 
     </div>
   )
 }
 
-export default  SortableElement(withStyles(styles)(UserColorBox));
+export default SortableElement(withStyles(styles)(UserColorBox))
