2d7163141351d32b5d20eb83113564859bd7fffb
solidstatejake <jberg3@binghamton.edu>
2020-03-08 00:16:50 -0500

    Security update and code formatting.

--- src/components/App.js
+++ src/components/App.js
@@ -1,57 +1,51 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Palette from "./Palette";
-import seedColors from "../utils/seedColors";
-import { generatePalette } from "../utils/colorHelpers";
-import '../stylesheets/css/main.css';
-import PaletteList from "./PaletteList";
-import SingleColorPalette from "./SingleColorPalette";
-import NewPalettePage from "./NewPalettePage";
+import React, { Component }                       from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Palette                                    from './Palette'
+import seedColors                                 from '../utils/seedColors'
+import { generatePalette }                        from '../utils/colorHelpers'
+import '../stylesheets/css/main.css'
+import PaletteList                                from './PaletteList'
+import SingleColorPalette                         from './SingleColorPalette'
+import NewPalettePage                             from './NewPalettePage'
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    const localStoragePalettes = JSON.parse(window.localStorage.getItem("palettes"));
-    const palettesToDisplay = localStoragePalettes || seedColors;
+  constructor (props) {
+    super(props)
+    const localStoragePalettes = JSON.parse(window.localStorage.getItem('palettes'))
+    const palettesToDisplay    = localStoragePalettes || seedColors
 
     this.state = {
-      palettes: [ ...palettesToDisplay ],
+      palettes:       [ ...palettesToDisplay ],
       palettesToKeep: seedColors.map(palette => palette.id)
-    };
+    }
 
-    this.createNewPalette = this.createNewPalette.bind(this);
-    this.deleteMiniPalette = this.deleteMiniPalette.bind(this);
+    this.createNewPalette  = this.createNewPalette.bind(this)
+    this.deleteMiniPalette = this.deleteMiniPalette.bind(this)
   }
 
-  findPalette(id) {
-    return this.state.palettes.find((palette) => {
-      return palette.id === id;
-    })
+  findPalette (id) {
+    return this.state.palettes.find(palette => palette.id === id)
   }
 
-  createNewPalette(newPalette) {
-    const { palettes } = this.state;
-    this.setState({
-        palettes: [ ...palettes, newPalette ]
-      },
+  createNewPalette (newPalette) {
+    this.setState(
+      { palettes: [ ...this.state.palettes, newPalette ] },
       this.syncLocalStorage
-    );
+    )
   }
 
-  deleteMiniPalette(id) {
-    const filteredPalettes = this.state.palettes.filter((palette) => {
-      return palette.id !== id
-    });
-
-    this.setState({ palettes: filteredPalettes})
+  deleteMiniPalette (id) {
+    const filteredPalettes = this.state.palettes.filter(palette => palette.id !== id)
+    this.setState({ palettes: filteredPalettes })
   }
 
-  syncLocalStorage() {
-    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
+  syncLocalStorage () {
+    window.localStorage.setItem('palettes', JSON.stringify(this.state.palettes))
   }
 
-  render() {
-    const { palettes, palettesToKeep } = this.state;
+  render () {
+    const { palettes, palettesToKeep } = this.state
 
     return (
       <Router>
@@ -59,8 +53,8 @@ class App extends Component {
           <Route
             exact path='/'
             render={ (routeProps) => <PaletteList palettes={ palettes }
-                                                  routeProps={routeProps}
-                                                  palettesToKeep={palettesToKeep}
+                                                  routeProps={ routeProps }
+                                                  palettesToKeep={ palettesToKeep }
                                                   deleteMiniPalette={ this.deleteMiniPalette }/> }
           />
 
@@ -72,29 +66,23 @@ class App extends Component {
 
           <Route
             exact path='/palette/:id'
-            render={ (routeProps) => (
-              <Palette
-                routeProps={ routeProps }
-                palette={ generatePalette(this.findPalette(routeProps.match.params.id)) }
-              />
-            ) }
+            render={ (routeProps) =>
+              <Palette routeProps={ routeProps }
+                       palette={ generatePalette(this.findPalette(routeProps.match.params.id)) }/> }
           />
 
           <Route
             exact path='/palette/:paletteId/:colorId'
-            render={ (routeProps) => (
-              <SingleColorPalette
-                routeProps={ routeProps }
-                palette={ generatePalette(this.findPalette(routeProps.match.params.paletteId)) }
-              />
-            ) }
+            render={ (routeProps) =>
+              <SingleColorPalette routeProps={ routeProps }
+                                  palette={ generatePalette(this.findPalette(routeProps.match.params.paletteId)) }/> }
           />
 
         </Switch>
       </Router>
 
-    );
+    )
   }
 }
 
-export default App;
+export default App
