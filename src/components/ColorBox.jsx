0821106a7d55dbaeb26dbff5a9004cce11ed3135
solidstatejake <jberg3@binghamton.edu>
2020-03-08 15:39:31 -0400

    Convert ColorBox comp to function from class.

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,62 +1,49 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { useState } from 'react'
+import { Link }            from 'react-router-dom'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-class ColorBox extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = { copied: false };
+const ColorBox = ({ background, displayShadesButton, id, name, paletteId }) => {
+  const [ copied, toggleCopiedState ] = useState(false)
 
-    this.changeCopyState = this.changeCopyState.bind(this);
+  const changeCopyState = () => {
+    toggleCopiedState(true)
+    setTimeout(() => toggleCopiedState(false), 700)
   }
 
-  changeCopyState() {
-    this.setState({ copied: true }, () => {
-      setTimeout(() => this.setState({ copied: false }), 1000);
-    })
-  }
+  return (
+    <CopyToClipboard text={ background } onCopy={ changeCopyState }>
 
-  render() {
-    const { name, background, id, paletteId } = this.props;
-    const { copied } = this.state;
-    return (
-      <CopyToClipboard text={ background } onCopy={ this.changeCopyState }>
+      <div className="ColorBox" style={ { background } }>
 
-        <div className="ColorBox" style={ { background } }>
-
-          <div className={ `ColorBox__overlay ${copied && 'ColorBox__active'}` }
-               style={ { background } }
-          >
-
-            <div
-              className={ `ColorBox__active--message ${copied && 'ColorBox__active--message-active'}` }>
-              <h1>Copied!</h1>
-              <p>{ background }</p>
-            </div>
-
+        <div className={ `ColorBox__overlay ${ copied && 'ColorBox__active' }` }
+             style={ { background } }>
+          <div className={ `ColorBox__active--message ${ copied
+                                                         && 'ColorBox__active--message-active' }` }>
+            <h1>Copied!</h1>
+            <p>{ background }</p>
           </div>
+        </div>
 
-          <button className="ColorBox__button--copy">Copy</button>
+        <button className="ColorBox__button--copy">Copy</button>
 
-          <span className='ColorBox__content--color-name'
-                style={ name.length > 18 ? { fontSize: '.8rem' } : {} }
-          >
+        <span className='ColorBox__content--color-name'
+              style={ name.length > 18 ? { fontSize: '.8rem' } : {} }
+        >
             { name }
-            </span>
+        </span>
 
-          { this.props.displayShadesButton &&
-          <Link to={ `/palette/${paletteId}/${id}` }
+        { displayShadesButton &&
+          <Link to={ `/palette/${ paletteId }/${ id }` }
                 onClick={ e => e.stopPropagation() }
                 className='ColorBox__button--see-more'>
             <span>Shades</span>
-          </Link>
-          }
-        </div>
+          </Link> }
+      </div>
 
-      </CopyToClipboard>
-    );
-  }
+    </CopyToClipboard>
+  )
+
 }
 
-export default ColorBox;
+export default ColorBox
