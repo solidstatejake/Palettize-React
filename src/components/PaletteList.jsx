4254c9098d36773754b7983ea36b74ea0f12bd9c
solidstatejake <jberg3@binghamton.edu>
2020-03-08 14:44:39 -0400

    Convert PaletteList to function from class.

--- src/components/PaletteList.jsx
+++ src/components/PaletteList.jsx
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
-import { Link }             from 'react-router-dom';
-import MiniPalette          from "./MiniPalette";
-import NavBar               from "./NavBar";
-
-class PaletteList extends Component {
+import React, { Component } from 'react'
+import { Link }             from 'react-router-dom'
+import MiniPalette          from './MiniPalette'
+import NavBar               from './NavBar'
 
 
-  render() {
-    const { palettes, routeProps, palettesToKeep } = this.props;
-    const miniPalettes = palettes.map(palette => (
-        <MiniPalette
-          key={ palette.id }
-          id={palette.id}
-          routeProps={routeProps}
-          palettesToKeep={palettesToKeep}
-          deleteMiniPalette={this.props.deleteMiniPalette}
-          { ...palette }
-        />
-    ));
+const PaletteList = ({ deleteMiniPalette, palettes, palettesToKeep, routeProps }) => {
 
-    return (
-      <div className='PaletteList'>
+  const miniPalettes = palettes.map(palette => (
+    <MiniPalette
+      key={ palette.id }
+      id={ palette.id }
+      routeProps={ routeProps }
+      palettesToKeep={ palettesToKeep }
+      deleteMiniPalette={ deleteMiniPalette }
+      { ...palette }
+    /> ))
 
-        <NavBar applyLeftMargin={true}>
-          <Link className='PaletteList__link--new-palette' to='/palette/new'>
-            <div>
-              Create New Palette
-            </div>
-          </Link>
-        </NavBar>
+  return (
+    <div className='PaletteList'>
 
-        <div className='PaletteList__container'>
-          { miniPalettes }
-        </div>
+      <NavBar applyLeftMargin={ true }>
+        <Link className='PaletteList__link--new-palette' to='/palette/new'>
+          <div>
+            Create New Palette
+          </div>
+        </Link>
+      </NavBar>
+
+      <div className='PaletteList__container'>
+        { miniPalettes }
       </div>
-    );
+    </div>
+  )
 
-  }
 }
 
-export default PaletteList;
+
+export default PaletteList
